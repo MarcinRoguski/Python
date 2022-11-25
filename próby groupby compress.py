6c2c3127a78897ba67b7bd08f31fa3853f4c7649
Marcin Roguski <111867908+MarcinRoguski@users.noreply.github.com>
2022-11-25 10:24:34 +0100

    Add files via upload

---
+++ próby groupby compress.py
@@ -0,0 +1,11 @@
+def compress(number):
+    from itertools import groupby
+    result = []
+    for key, group in groupby(str(number)):
+        result.append(key + len(list(group)) * ".")
+    result2 = ''.join(result)
+    return (result2)
+
+
+# uruchamia funkcję i grupuje cyfry ilość cyfr zastępując kropką
+print(compress(11225555555555))
